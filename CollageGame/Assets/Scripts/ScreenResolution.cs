6623cf0cd9caf8170b5ef1dad33c278bef901b4b
Vicky3252 <veekshith02kolanupaka@gmail.com>
2021-12-14 18:49:57 +0530

    A bit of an update

--- CollageGame/Assets/Scripts/ScreenResolution.cs
+++ CollageGame/Assets/Scripts/ScreenResolution.cs
@@ -1,11 +1,11 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScreenResolution : MonoBehaviour
 {
-    List<int> widths = new List<int>() { 800, 1280, 1600, 1920 };
-    List<int> heights = new List<int>() { 600, 720, 900, 1080 };
+    List<int> widths = new List<int>() { 1280, 1366, 1600, 1920 };
+    List<int> heights = new List<int>() { 720, 768, 900, 1080 };
     public void SetRes(int index)
     {
         bool fullscreen = Screen.fullScreen;
